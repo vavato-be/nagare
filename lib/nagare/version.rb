fe5f1816f65869d1ecba04ee34c4eb290e2823d4
Alex Reis <alex@alexmreis.com>
2021-07-13 17:22:03 +0200

    feat: Increment version number automaticaly on release

--- lib/nagare/version.rb
+++ lib/nagare/version.rb
@@ -2,6 +2,9 @@
 
 # rubocop:disable
 module Nagare
+  # Needs to be double quotes for release-please ruby updater
+  # see https://github.com/googleapis/release-please/blob/master/src/updaters/version-rb.ts
+  #
   # rubocop:disable Style/StringLiterals
   VERSION = "0.3.0"
   # rubocop:enable Style/StringLiterals
